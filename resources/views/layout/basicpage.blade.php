8c3fb0b733fcab27d8c5bdae4616bb535969bc43
Nicolas Da Mutten <nicolas@damutten.ch>
2018-08-24 11:51:24 +0200

    Sets up laravel-mix

--- resources/views/layout/basicpage.blade.php
+++ resources/views/layout/basicpage.blade.php
@@ -5,6 +5,10 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>{{ config('app.name') }}</title>
+        <link rel="stylesheet" href="{{ mix('css/app.css') }}" type="text/css" charset="utf-8">
+        <script src="{{ mix('js/manifest.js') }}" defer></script>
+        <script src="{{ mix('js/vendor.js') }}" defer></script>
+        <script src="{{ mix('js/app.js') }}" defer></script>
     </head>
     <body>
         <main class="container pb-3">
--- webpack.mix.js
+++ webpack.mix.js
@@ -12,4 +12,12 @@ let mix = require('laravel-mix');
  */
 
 mix.js('resources/assets/js/app.js', 'public/js')
-   .sass('resources/assets/sass/app.scss', 'public/css');
+   .sass('resources/assets/sass/app.scss', 'public/css')
+   .extract(['bootstrap'])
+   .sourceMaps(false);
+
+if (!mix.inProduction()) {
+    mix.webpackConfig({
+        devtool: 'inline-source-map',
+    });
+}
