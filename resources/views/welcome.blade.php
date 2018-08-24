f72c4ecd696296f2542db800bcc3572c2144f831
Nicolas Da Mutten <nicolas@damutten.ch>
2018-08-24 11:50:56 +0200

    Sets up basicpage-template

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,11 +1,5 @@
-<!DOCTYPE html>
-<html lang="{{ app()->getLocale() }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>{{ config('app.name') }}</title>
-    </head>
-    <body>
-    </body>
-</html>
+@extends('layout.basicpage')
+
+@section('content')
+
+@endsection
